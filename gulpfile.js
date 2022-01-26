4ad72467bfce885ce1e0c5cf32c8e3af9544dbd9
rootyss <jaxent@yandex.ru>
2022-01-26 11:00:51 +0300

    add gulp-sass, gulp-sourcemaps, gulp-rename, gulp-clean, gulp-clean-css, gulp-autoprefixer, browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, series, dest, watch} = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+const cleancss = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+const cleanDist = () => src('./dist', {read: false}).pipe(clean());
+
+const build = () =>  {
+  return src('./app/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(['cover 99.5%']))
+    .pipe(cleancss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write())
+    .pipe(dest('./dist'));
+};
+
+const html = () => src('./app/index.html').pipe(dest('./dist'));
+
+const serve = () => {
+  browserSync.init({
+        server: "./dist"
+    });
+  watch('./app/*.scss', series(cleanDist, html, build)).on('change', browserSync.reload)
+};
+
+exports.serve = serve;
+exports.build = build;
